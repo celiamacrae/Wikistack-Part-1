34f2d4d13c0e2907b74ed4f2808b764fdebd41b9
Celia Macrae <celiamacrae@gmail.com>
2019-11-14 17:34:24 -0500

    Querying wth models

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const {addPage} = require("../views");
 const bodyParser = require("body-parser");
+const {Page} = require("../models");
 
 //OR const addPage = require("../views/addPage");
 
@@ -10,27 +11,30 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.urlencoded({extended: false}));
 
 
-router.get('/', function (req, res, next){
+router.get('/', async (req, res, next) => {
   res.redirect('/');
 })
 
-router.post('/', function(req,res,next){
-  res.red('/');
+router.post('/', async (req,res,next) => {
+
+  const page = new Page({
+    title: res.json(req.body.title),
+    content: res.json(req.body.content)
+  })
+
+  try{
+    await page.save();
+    res.redirect('/');
+  }
+  catch(error){
+    next(error)
+  }
+
 })
 
-router.get('/add', function(req,res,next){
+router.get('/add', async (req,res,next) => {
   res.send(addPage());
+
 })
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
--- views/addPage.js
+++ views/addPage.js
@@ -5,11 +5,22 @@ module.exports = () => layout(html`
   <h3>Add a Page</h3>
   <hr>
   <form method="POST" action="/wiki/">
-    
-    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
-    
-    <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
-    
+
+
+    <div class="form-group">
+      <label for="author_name" class="col-sm-2 control-label">Author Name</label>
+      <div class="col-sm-10">
+        <input id="author_name" name="author_name" type="text" class="form-control"/>
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label for="author_email" class="col-sm-2 control-label">Email</label>
+      <div class="col-sm-10">
+        <input id="author_email" name="author_email" type="email" class="form-control"/>
+      </div>
+    </div>
+
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
       <div class="col-sm-10">
@@ -17,13 +28,22 @@ module.exports = () => layout(html`
       </div>
     </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
-    
-    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+    <div class="form-group">
+      <label for="content" class="col-sm-2 control-label">Content</label>
+      <textarea rows="10" cols="100"></textarea>
+    </div>
+
+    <div class="form-group">
+      <label for="status" class="col-sm-2 control-label">Status</label>
+      <div class="col-sm-10">
+        <input id="status" name="status" type="radio" class="form-control"/>Closed<br>
+        <input id="status" name="status" type="radio" class="form-control"/>Open<br>
+      </div>
+    </div>
 
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">submit</button>
     </div>
-  
+
   </form>
 `);
