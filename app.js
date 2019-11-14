4c0147c6e2ec6d3f48950c5398aaacf7bc16bad5
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-11-14 12:13:53 -0500

    view modules add for everything

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const layout = require("./views/layout");
+
 
 const app = express();
 
@@ -10,4 +12,15 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-'TRY'
+
+app.get("/", (req, res) => {
+     res.send(layout(""));
+
+
+});
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
